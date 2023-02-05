9b5503ad34d33b60921f4cd03b50cf074d5e01cc
aniket264538 <ajha264538@gmail.com>
2023-02-05 23:26:55 +0530

    Category Dto and Controller Small changes

--- thesocialapp/src/main/java/com/socialapp/controllers/CategoryController.java
+++ thesocialapp/src/main/java/com/socialapp/controllers/CategoryController.java
@@ -1,24 +1,15 @@
 package com.socialapp.controllers;
 
-import java.util.List;
-
-import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.socialapp.payloads.ApiResponse;
 import com.socialapp.payloads.CategoryDto;
 import com.socialapp.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/categories")
@@ -32,6 +23,14 @@ public class CategoryController {
 
 		CategoryDto createCategory = this.categoryService.createCategory(categoryDto);
 		return new ResponseEntity<CategoryDto>(createCategory, HttpStatus.OK);
+	}
+
+	@GetMapping("")
+	public ResponseEntity<List<CategoryDto>> getCategories()
+	{
+		List<CategoryDto> categoryById = this.categoryService.getallCategory();
+		return ResponseEntity.ok(categoryById);
+
 	}
 
 	@PutMapping("/{categoryId}")
@@ -60,13 +59,7 @@ public class CategoryController {
 		
 	}
 	
-	@GetMapping("/")
-	public ResponseEntity<List<CategoryDto>> getCategories()
-	{
-		List<CategoryDto> categoryById = this.categoryService.getallCategory();
-		return ResponseEntity.ok(categoryById);
-		
-	}
+
 
 	
 	
--- thesocialapp/src/main/java/com/socialapp/payloads/CategoryDto.java
+++ thesocialapp/src/main/java/com/socialapp/payloads/CategoryDto.java
@@ -1,11 +1,11 @@
 package com.socialapp.payloads;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
-
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @NoArgsConstructor
 @Getter @Setter
@@ -14,10 +14,10 @@ public class CategoryDto {
 	
 	private Integer categoryId;
 	
-	@NotBlank @Size(min = 4,message = "Minimun size of title should be 4")
+	@NotBlank @Size(min = 4,message = "Minimum size of title should be 4")
 	private String categoryTitle;
 	
-	@NotBlank @Size(min = 4,message = "Minimun size of description should be 10")
+	@NotBlank @Size(min = 4,message = "Minimum size of description should be 10")
 	private String categoryDescription;
 	
 	
